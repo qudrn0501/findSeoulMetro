e628de5debb9768b71e6ddd12cd3841696939f72
qudrn0501 <104612845+qudrn0501@users.noreply.github.com>
2024-09-13 17:01:39 +0900

    Beta v1.0.1 (정답 모달 추가/ UI 변경 및 개선 / 반응형 일부 적용)

---
+++ src/component/Modal/Modal.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import styles from './Modal.module.css';
+
+const Modal = ({ stationName, onClose }) => {
+
+  const handleCloseClick = () => {
+    onClose(); // 모달 닫기 함수 호출
+  }
+
+  return (
+    <div className={styles.dim} onClick={handleCloseClick}>
+      <div className={`${styles.modal} ${styles.modal_location}`} onClick={(e) => e.stopPropagation()}>
+        <div className={styles.correct_modal}>
+          {/* 클릭한 역명을 받아와 표시 */}
+          <h1>{stationName}역, 정답입니다.</h1>
+          <p>다시 시작하려면 창을 닫고 <br />
+            GO! 버튼을 눌러주세요.
+          </p>
+          <button
+           onClick={handleCloseClick}
+           className={styles.btn_close}
+          >닫기</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Modal;
--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState, useRef } from 'react';
 import styles from "./Home.module.css";
 import Map from "../../component/Map/Map";
+import Modal from "../../component/Modal/Modal";
 import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch';
 
 const Home = () => {
@@ -11,6 +12,8 @@ const Home = () => {
   const [time, setTime] = useState(0); // 타이머 시간
   const [isRunning, setIsRunning] = useState(false); // 타이머 작동 여부
   const timerRef = useRef(null); // 타이머 참조용
+  const [isModalOpen, setIsModalOpen] = useState(false); // 모달 창 열기/닫기 관리
+  const [stationName, setStationName] = useState(''); // 클릭한 역 이름 관리
   const API_KEY = process.env.REACT_APP_API_KEY;
   const url = `http://openapi.seoul.go.kr:8088/${API_KEY}/json/SearchSTNBySubwayLineInfo/1/1000/`;
 
@@ -40,7 +43,6 @@ const Home = () => {
     const formatTime = (totalSeconds) => {
       const minutes = Math.floor(totalSeconds / 60);
       const seconds = totalSeconds % 60;
-  
       return totalSeconds < 60 ? `${seconds}초` : `${minutes}분 ${seconds}초`;
     };
 
@@ -50,6 +52,8 @@ const Home = () => {
       let randomIndex = Math.floor(Math.random() * stationNumber) // 역의 총 갯수 중 정수 형태의 난수 1개
       let randomStationCD = data[randomIndex]?.STATION_CD; // 결정된 난수의 역의 코드번호 호출
       setSelectedStationCD(randomStationCD); // 역 코드의 상태 변화
+      console.log(stationNumber, randomIndex, randomStationCD);
+      setStationName(data.find((station) => station.STATION_CD === randomStationCD)?.STATION_NM); // 역 이름 저장소 상태 변화
   
       // 타이머 초기화 및 시작
       setTime(0);
@@ -57,12 +61,19 @@ const Home = () => {
       setIsStationSelected(false); // 버튼 비활성화 (역을 클릭하기 전까지)
     };
 
+    // 역 클릭 시 처리
     const handleStnClick = (station) => {
-      // 역 버튼 눌렀을 시 타이머 멈추기
       console.log("Correct");
       setIsRunning(false); // 스톱워치 멈추기
       setIsStationSelected(true); // 역 클릭 시 랜덤 버튼 다시 활성화
-      alert(`${station.STATION_NM}역을 찾았습니다⭕`); // 정답 확인
+      // alert(`${station.STATION_NM}역을 찾았습니다⭕`);
+      setIsModalOpen(true); // 모달 열기
+      setStationName(station.STATION_NM); // 클릭한 역 이름 저장
+    };
+
+    // 모달 닫기
+    const handleCloseModal = () => {
+      setIsModalOpen(false);
     };
   
   // const handleClick = (e) => {
@@ -91,27 +102,31 @@ const Home = () => {
         )}
       </div>
       <div className={styles.important}>
-
-      <TransformWrapper
-        initialScale={1.3}      // 초기 스케일을 부모 요소에 맞게 조정
-        minScale={1.3}          // 최소 스케일을 0.1로 설정 (너무 축소되지 않도록)
-        maxScale={5}            // 최대 스케일 설정
-        centerZoomedOut={true}     // 초기 로드 시 중앙에 배치
-        limitToBounds={true}    // 경계 내에서만 드래그 가능하게 제한
-      >
-        <TransformComponent>
-          <div className={styles.background_image}>
-            <Map 
-              data={data}
-              selectedStationCD={selectedStationCD}
-              onStationClick={handleStnClick}
-            />
-          </div>
-        </TransformComponent>
-      </TransformWrapper>
+        <TransformWrapper
+          initialScale={1.3}      // 초기 스케일을 부모 요소에 맞게 조정
+          minScale={1.3}          // 최소 스케일을 0.1로 설정 (너무 축소되지 않도록)
+          maxScale={5}            // 최대 스케일 설정
+          centerZoomedOut={true}     // 초기 로드 시 중앙에 배치
+          limitToBounds={true}    // 경계 내에서만 드래그 가능하게 제한
+        >
+          <TransformComponent>
+            <div className={styles.background_image}>
+              <Map 
+                data={data}
+                selectedStationCD={selectedStationCD}
+                onStationClick={handleStnClick}
+              />
+            </div>
+          </TransformComponent>
+        </TransformWrapper>
+      </div>
+      {isModalOpen && (
+        <Modal 
+          stationName={stationName} // 클릭된 역 이름 전달
+          onClose={handleCloseModal} // 모달 닫기 함수 전달
+        />
+      )}
     </div>
-    </div>
-    
   )
 }
 

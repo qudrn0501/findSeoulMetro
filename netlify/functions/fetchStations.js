6181363cf120364fc68add62f130318f585a44c2
qudrn0501 <104612845+qudrn0501@users.noreply.github.com>
2024-09-17 00:01:45 +0900

    fetchStation import 동적 호출

--- netlify/functions/fetchStations.js
+++ netlify/functions/fetchStations.js
@@ -1,16 +1,15 @@
-const fetch = require('node-fetch');
-
 exports.handler = async function(event, context) {
-  const API_KEY = process.env.API_KEY;  // 환경변수에서 API_KEY 가져오기
+  const fetch = (await import('node-fetch')).default;  // 동적 import 사용
+  const API_KEY = process.env.API_KEY;
   const apiUrl = `https://openapi.seoul.go.kr:8088/${API_KEY}/json/SearchSTNBySubwayLineInfo/1/1000/`;
 
   try {
     const response = await fetch(apiUrl);
-    
+
     if (!response.ok) {
       throw new Error(`API request failed with status ${response.status}`);
     }
-    
+
     const data = await response.json();
 
     return {
@@ -19,7 +18,7 @@ exports.handler = async function(event, context) {
       headers: {
         "Access-Control-Allow-Origin": "*",  // CORS 설정
         "Access-Control-Allow-Headers": "Content-Type",
-      }
+      },
     };
   } catch (error) {
     console.error('Error fetching data:', error);
